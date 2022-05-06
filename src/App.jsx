42979eb0d28f39f262983691b021b77c717ee55a
Dayan Arango <dayaara6@gmail.com>
2022-05-06 13:28:23 -0400

    Revert "estados de busqueda"
    This reverts commit e4153a21f606169302471069dd91aab5a54ec455.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import AppClima from "./components/AppClima";
-import { ClimaProvider } from "./context/ClimaProvider";
+import AppClima from "./components/AppClima"
+
 
 function App() {
+
   return (
-    <ClimaProvider>
-      <header><h1>Buscador de Clima</h1></header>
-      {" "}
-      <AppClima />
-    </ClimaProvider>
-  );
+    <AppClima />
+  )
 }
 
-export default App;
+export default App
